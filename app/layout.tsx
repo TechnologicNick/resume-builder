00ce0fc4e3c53126d30e8e005e34e169a01b0190
TechnologicNick <technologicnick@gmail.com>
2024-09-08 01:46:09 +0200

    Remove border

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,7 @@
 import { SandpackCSS } from "@/components/sandpack-styles";
 
+import "./global.css";
+
 export const metadata = {
   title: "ResumeSX",
   description: "Fine-tune your resume in React",
