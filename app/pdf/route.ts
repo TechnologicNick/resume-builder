3a80fc4ea1b2c38c4dcf44716600db5a9495a725
TechnologicNick <technologicnick@gmail.com>
2024-09-10 00:21:36 +0200

    Add closing page on stream close and reuse browser across hot reloads

--- app/pdf/route.ts
+++ app/pdf/route.ts
@@ -1,8 +1,14 @@
 import type { NextRequest } from "next/server";
-import puppeteer from "puppeteer";
+import puppeteer, { Browser, Page, PDFOptions } from "puppeteer";
 
-const browser = puppeteer.launch({
-  headless: true,
+declare global {
+  // Create a global browser instance that can be reused across requests
+  // and hot reloads
+  var browser: Promise<Browser>;
+}
+
+globalThis.browser ??= puppeteer.launch({
+  headless: "shell",
   pipe: true,
   args: [
     "--no-sandbox",
@@ -12,24 +18,57 @@ const browser = puppeteer.launch({
   dumpio: true,
 });
 
+async function createPDFStreamResponse(
+  page: Page,
+  options: PDFOptions,
+  onClose: () => Promise<void>
+) {
+  try {
+    const pdfStream = await page.createPDFStream(options);
+
+    const streamWithCloseHandler = pdfStream.pipeThrough(
+      new TransformStream({
+        flush(controller) {
+          onClose().catch(console.error);
+        },
+      })
+    );
+
+    return new Response(streamWithCloseHandler, {
+      headers: { "Content-Type": "application/pdf" },
+    });
+  } catch (error) {
+    console.error("Error generating PDF:", error);
+    await page.close();
+    throw error;
+  }
+}
+
 export async function POST(req: NextRequest) {
   const html = await req.text();
 
   let page = await (await browser).newPage();
   page.setJavaScriptEnabled(false);
 
+  setTimeout(() => {
+    // Prevent memory leak if someone keeps the stream open
+    if (!page.isClosed()) {
+      page.close();
+    }
+  }, 10000);
+
   await page.setContent(html, {
     timeout: 1000,
   });
 
-  const stream = await page.createPDFStream({
-    omitBackground: true,
-    format: "A4",
-  });
-
-  return new Response(stream, {
-    headers: {
-      "Content-Type": "application/pdf",
+  return createPDFStreamResponse(
+    page,
+    {
+      omitBackground: true,
+      format: "A4",
     },
-  });
+    async () => {
+      await page.close();
+    }
+  );
 }
