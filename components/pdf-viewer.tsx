1848679b2a64751c4645ba163be2501c712510c8
TechnologicNick <technologicnick@gmail.com>
2024-09-18 03:20:33 +0200

    Add annotation layer and fix blinking PDF on update

--- components/pdf-viewer.tsx
+++ components/pdf-viewer.tsx
@@ -1,6 +1,5 @@
-import { useEffect, useState } from "react";
+import { memo, useCallback, useEffect, useState } from "react";
 import { Document, Page, pdfjs } from "react-pdf";
-import type { PDFDocumentProxy } from "pdfjs-dist/types/src/display/api";
 
 import "react-pdf/dist/Page/AnnotationLayer.css";
 import "react-pdf/dist/Page/TextLayer.css";
@@ -11,11 +10,11 @@ const options: React.ComponentProps<typeof Document>["options"] = {
   cMapUrl: `https://cdn.jsdelivr.net/npm/pdfjs-dist@${pdfjs.version}/cmaps/`,
 };
 
+/**
+ * PDFViewer component that listens for messages from the parent window
+ */
 export const PDFViewer = () => {
-  const [file, setFile] = useState<File | null>(null);
-  const [lastLoadedPdf, setLastLoadedPdf] = useState<null | PDFDocumentProxy>(
-    null
-  );
+  const [files, setFiles] = useState<{ key: string; file: File }[]>([]);
 
   useEffect(() => {
     function handleMessage(event: MessageEvent<any>) {
@@ -27,10 +26,19 @@ export const PDFViewer = () => {
         return;
       }
 
-      setFile(
-        new File([event.data.buffer], "document.pdf", {
-          type: "application/pdf",
-        })
+      const file = new File([event.data.buffer], "document.pdf", {
+        type: "application/pdf",
+      });
+
+      setFiles((files) =>
+        [
+          {
+            key: crypto.randomUUID(),
+            file,
+          },
+        ]
+          .concat(files)
+          .slice(0, 2)
       );
     }
 
@@ -41,23 +49,72 @@ export const PDFViewer = () => {
     };
   }, []);
 
+  const onLoadSuccessReal = useCallback((removeFilesOlderThan: File) => {
+    setFiles((currentFiles) => {
+      let index = 0;
+      for (let i = 0; i < currentFiles.length; i++) {
+        if (currentFiles[i].file === removeFilesOlderThan) {
+          index = i;
+          break;
+        }
+      }
+
+      return currentFiles.slice(0, index + 1);
+    });
+  }, []);
+
   return (
     <div className="pdf-container">
-      {lastLoadedPdf &&
-        Array.from(new Array(lastLoadedPdf.numPages), (_, index) => (
-          <Page
-            key={`page_${index + 1}`}
-            pageNumber={index + 1}
-            pdf={lastLoadedPdf}
-            renderAnnotationLayer={false}
-          />
-        ))}
-
-      <Document
-        file={file}
-        onLoadSuccess={(pdf) => setLastLoadedPdf(pdf)}
-        options={options}
-      />
+      {files.map(({ key, file }) => (
+        <PDF key={key} file={file} onLoadSuccessReal={onLoadSuccessReal} />
+      ))}
     </div>
   );
 };
+
+/**
+ * Wrapper around react-pdf's Document component to handle loading state
+ */
+const PDF = memo(
+  ({
+    file,
+    onLoadSuccessReal,
+  }: {
+    file: File;
+    onLoadSuccessReal?: (file: File) => void;
+  }) => {
+    const [numPages, setNumPages] = useState(0);
+    const [loading, setLoading] = useState(true);
+
+    return (
+      <Document
+        file={file}
+        onLoadSuccess={(pdf) => {
+          setNumPages(pdf.numPages);
+
+          // I swear this is the only way to not have the page blink on load
+          requestAnimationFrame(() =>
+            requestAnimationFrame(() =>
+              requestAnimationFrame(() =>
+                requestAnimationFrame(() =>
+                  requestAnimationFrame(() =>
+                    requestAnimationFrame(() => {
+                      setLoading(false);
+                      onLoadSuccessReal?.(file);
+                    })
+                  )
+                )
+              )
+            )
+          );
+        }}
+        options={options}
+        className={loading ? "hidden" : ""}
+      >
+        {Array.from(new Array(numPages), (_, index) => (
+          <Page key={`page_${index + 1}`} pageNumber={index + 1} />
+        ))}
+      </Document>
+    );
+  }
+);
